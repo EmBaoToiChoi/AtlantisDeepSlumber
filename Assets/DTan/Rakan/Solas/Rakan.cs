b3d362b20ad9565646fa376999d5f8a57776806e
NguyenDuyTan <duytannguyen750@gmail.com>
2026-06-15 23:45:55 +0700

    UpdateCutscene1

--- Assets/DTan/Rakan/Solas/Rakan.cs
+++ Assets/DTan/Rakan/Solas/Rakan.cs
@@ -11,5 +11,25 @@ public class Rakan : MonoBehaviour
     {
         animator.SetTrigger("QuyThem");
     }
+    public void SetTriggerStand()
+    {
+        animator.SetTrigger("STA");
+    }
+    public void SetTriggerNHQ()
+    {
+        animator.SetTrigger("NHQ");
+    }
+    public void SetTriggerTHR()
+    {
+        animator.SetTrigger("THR");
+    }
+    public void SetTriggerTK()
+    {
+        animator.SetTrigger("TK");
+    }
+    public void SetTriggerID()
+    {
+        animator.SetTrigger("ID");
+    }
 
 }
