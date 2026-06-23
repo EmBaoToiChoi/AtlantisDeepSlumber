4e68864643d9a88946110524a9916f9d6e7f11f7
NguyenDuyTan <duytannguyen750@gmail.com>
2026-06-24 00:42:12 +0700

    UpdateCutscene1

---
+++ Assets/DTan/PlayerHR/PlayerRun.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class PlayerRun : MonoBehaviour
+{
+        public Animator animator;
+        public void TriggerDi()
+    {
+        animator.SetTrigger("Di");
+    }
+    public void TriggerGatDau()
+    {
+        animator.SetTrigger("GatDau");
+    }
+    public void TriggerIdle()
+    {
+        animator.SetTrigger("DungYen");
+    }
+
+}
