b75b050fd44d35d13b8e011f8a3aa0d712cafbde
EmBaoToiChoi <hoaibao200611423@gmail.com>
2026-06-27 13:06:44 +0700

    1

--- Assets/Hbao/PLayer4_Athurt/Scrip/ArthurPlayer.cs
+++ Assets/Hbao/PLayer4_Athurt/Scrip/ArthurPlayer.cs
@@ -3882,8 +3882,13 @@ public class ArthurPlayer : NetworkBehaviour, IPlayerHUDTarget
             if (collectible != null) collectible.ConfirmCollect();
             else
             {
-                var repair = pendingPickItem.GetComponent<RepairItemDrop>();
-                if (repair != null) repair.ConfirmCollect();
+                var axe = pendingPickItem.GetComponent<AxeItem>();
+                if (axe != null) axe.ConfirmPickup(gameObject);
+                else
+                {
+                    var repair = pendingPickItem.GetComponent<RepairItemDrop>();
+                    if (repair != null) repair.ConfirmCollect();
+                }
             }
             pendingPickItem = null;
         }
