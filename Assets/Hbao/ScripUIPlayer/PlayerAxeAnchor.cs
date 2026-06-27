9ed885223a176780015d648585a6724ea0b607f7
EmBaoToiChoi <hoaibao200611423@gmail.com>
2026-06-27 10:38:43 +0700

    Create PlayerAxeAnchor component

---
+++ Assets/Hbao/ScripUIPlayer/PlayerAxeAnchor.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class PlayerAxeAnchor : MonoBehaviour
+{
+    [Header("Axe Transform Offsets for this Character")]
+    public Vector3 positionOffset = Vector3.zero;
+    public Vector3 rotationOffset = Vector3.zero;
+    public Vector3 scaleOffset = Vector3.one;
+}
